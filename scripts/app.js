46a57bffba2a354f602435b66c9d36916d5105cf
Edwin574 <owinoedwin574@gmail.com>
2023-02-03 06:33:41 +0300

    basic functionality complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,20 @@
 const ratingButton=document.querySelectorAll(".value")
-
+const rateValue=document.getElementById("rate-value")
+const button=document.querySelector(".submit")
 console.log(ratingButton)
+
+let rating
+
+ratingButton.forEach((element)=>{
+    
+    element.addEventListener('click',(e)=>{
+        element.classList.add('value-clicked')
+        let value
+        value=e.target.innerText
+        rating=value
+    })
+})
+
+button.addEventListener('click',()=>{
+rateValue.innerHTML=rating
+})
